0a73072295d391c0c298979eaa906370e1c1aaa4
Rizal Farhan <rizlafarhannn@gmail.com>
2025-05-10 21:01:06 +0800

    perubahan sedikit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
   // Other config options...
+  integrations: [],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
